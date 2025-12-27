f063d79c0f1328a5728c952e461378d8962d4e49
Udayveer525 <152760915+Udayveer525@users.noreply.github.com>
2025-12-27 16:31:13 +0530

    first

---
+++ app/(root)/index.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { Text, View, FlatList, Alert, Image, TouchableOpacity, RefreshControl} from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { useTransactions } from "../../hooks/useTransactions";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "@/assets/styles/home.styles";
+import { SignOutButton } from "@/components/SignOutButton";
+import PageLoader from "@/components/PageLoader";
+import { BalanceCard } from "@/components/BalanceCard";
+import { TransactionItem } from "@/components/TransactionItem";
+import NoTransactionFound from "@/components/NoTransactionFound";
+
+
+export default function Page() {
+  const { user } = useUser();
+  const router = useRouter();
+  const [refreshing, setRefreshing] =  useState(false);
+
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(user.id);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadData();
+    setRefreshing(false);
+  }
+
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  const handleDelete = (id) => {
+    Alert.alert("Delete Transaction", "Are you sure you want to delete this transaction?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => deleteTransaction(id),
+      },
+    ]);
+  }
+
+  if (isLoading && !refreshing) return <PageLoader />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {/* Header */}
+        <View style={styles.header}>
+          {/* Left  */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeTest}> Welcome, </Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          {/* Right  */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}> ADD </Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionListContent}
+        data={transactions}
+        renderItem={({ item }) => (
+          <TransactionItem item={item} onDelete={handleDelete} />
+        )}
+        ListEmptyComponent={<NoTransactionFound /> }
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
+    </View>
+  );
+}
---
+++ constants/api.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://spendx.onrender.com/api";
+//const API_URL = "http://localhost:5001/api";
+
+
